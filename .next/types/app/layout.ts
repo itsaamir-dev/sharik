20fd50632ad093be42c09e86d5e93267ce39cb69
huzuhuzair <ba.aamir@ebisuinc.net>
2026-05-06 23:17:13 +0530

    bug fix

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: A:\coding\SharikRasool--NextJs\src\app\layout.tsx
+// File: /Users/aamirbashir/Office/SHARUS_WEB_Fork/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: A:\coding\SharikRasool--NextJs\src\app\page.tsx
+// File: /Users/aamirbashir/Office/SHARUS_WEB_Fork/src/app/page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
